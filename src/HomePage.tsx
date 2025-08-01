021f15faa9d4814ad9b882b656d5f75dac0a7f5a
mohiyuddin <mohiyuddin.shaikh@tradofina.com>
2025-08-01 14:00:17 +0530

    wihotut tray

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -2,13 +2,13 @@ import "./App.css";
 
 function HomePage() {
   const handlePaytmClick = () => {
-    const upiLink = `upi://pay?pa=8898440622@pthdfc&pn=Test%20Merchant&am=1&cu=INR&tn=Test%20Payment`;
-    window.location.href = upiLink;
+    const intentLink = `intent://pay?pa=8898440622@pthdfc&pn=Test%20Merchant&am=1&cu=INR&tn=Test%20Payment#Intent;scheme=upi;package=net.one97.paytm;end;`;
+    window.location.href = intentLink;
   };
 
   return (
     <div style={{ padding: "2rem" }}>
-      <button onClick={handlePaytmClick}>Pay with Paytm UPI</button>
+      <button onClick={handlePaytmClick}>Pay with Paytm intent</button>
     </div>
   );
 }
