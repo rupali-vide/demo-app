82a3c27cc75a03626820a68b47f07b9ab2eed1fb
mohiyuddin <mohiyuddin.shaikh@tradofina.com>
2025-08-01 13:57:44 +0530

    added my upi id as test

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 function HomePage() {
   const handlePaytmClick = () => {
-    const upiLink = `upi://pay?pa=testmerchant@paytm&pn=Test%20Merchant&am=1&cu=INR&tn=Test%20Payment`;
+    const upiLink = `upi://pay?pa=8898440622@pthdfc&pn=Test%20Merchant&am=1&cu=INR&tn=Test%20Payment`;
     window.location.href = upiLink;
   };
 
